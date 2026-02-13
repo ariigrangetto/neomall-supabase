4ac3a8aa62f6aae774eab417d2af7af881ae19d0
Arianna Grangetto <ariigrangetto5@gmail.com>
2026-02-13 11:15:30 -0300

    feat: new links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <>
       <h1>Welcome to Neomall</h1>
-      <Link to={"/login"}>Login</Link>
+      <Link to='/login'>Login </Link>
+      <Link to='/register'>Register</Link>
     </>
   );
 }
