728d4e6d54e5f4cff5265e0b6f5561a59efe6c9c
Arianna Grangetto <ariigrangetto5@gmail.com>
2026-02-13 11:15:03 -0300

    feat: protected route, lazy and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,38 @@ import Login from "./pages/Login.tsx";
 import Home from "./pages/Home.tsx";
 import Products from "./pages/Products.tsx";
 import Register from "./pages/Register.tsx";
+import { ProtectedRoute } from "./components/ProtectedRoute.tsx";
+import Profile from "./pages/Profile.tsx";
+import { lazy, Suspense } from "react";
+import Loading from "./pages/Loading.tsx";
+
+const Details = lazy(() => import("./pages/Details.tsx"));
+const NotFound = lazy(() => import("./pages/404.tsx"));
+const Cart = lazy(() => import("./pages/Cart.tsx"));
+const Products = lazy(() => import("./pages/Products.tsx"));
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />} />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path='/' element={<Home />} />
 
-        <Route path='/login' element={<Login />} />
-        <Route path='/Register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/Register' element={<Register />} />
 
-        <Route path='/products' element={<Products />} />
-      </Routes>
+          <Route
+            path='/profile'
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
