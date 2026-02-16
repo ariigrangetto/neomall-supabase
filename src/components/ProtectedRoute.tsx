d0848cc1a3a4bf8a9248325f041132974cf135bd
Arianna <ariigrangetto5@gmail.com>
2026-02-16 19:32:33 -0300

    feat: protected route component

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,34 +1,12 @@
-import { useEffect, useState } from "react";
-import useLoading from "../hooks/useLoading";
-import { useNavigate } from "react-router";
-import supabase from "../supabase/client.js";
+import useAuth from "../hooks/useAuth.js";
 
 interface Props {
   children: React.ReactNode;
 }
 export const ProtectedRoute = ({ children }: Props) => {
-  const { loading, setLoading } = useLoading();
-  const [authenticated, setAuthenticated] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setLoading(true);
-    const checkAuth = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      if (!session) {
-        navigate("/login");
-      } else {
-        setAuthenticated(true);
-      }
-      setLoading(false);
-    };
-
-    checkAuth();
-  }, [navigate]);
+  const { isAuthenticated, loading } = useAuth();
 
   if (loading) return <p>Cargando sesión...</p>;
 
-  return authenticated ? <>{children}</> : null;
+  return isAuthenticated ? <>{children}</> : null;
 };
