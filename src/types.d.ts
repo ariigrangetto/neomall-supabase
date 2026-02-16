2585f0dfa347a3a784c4f97f18fd2702e070cab4
Arianna <ariigrangetto5@gmail.com>
2026-02-16 19:31:35 -0300

    feat: typescript types

---
+++ src/types.d.ts
@@ -0,0 +1,41 @@
+export interface FiltersType {
+  text: string;
+  category: string;
+}
+
+export interface CartItem {
+  id: number | string;
+  quantity: number;
+  product_id: number | string;
+  cart_id: number | string;
+}
+
+export interface Products {
+  id: number | string;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discount: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  warraty: string;
+  shipping: string;
+  availability: string;
+}
+
+export interface Cart {
+  id: number | string;
+  user_id: number | string;
+}
+
+export interface Rating {
+  id: number | string;
+  rating: number;
+  comment: string;
+  reviewerName: string;
+  reviewerEmail: string;
+  created_at: string;
+  product_id: number | string;
+}
