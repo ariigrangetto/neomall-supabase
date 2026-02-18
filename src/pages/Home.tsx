d79dee00541ae36be35c5897280b002ac1ea5eb6
Arianna <ariigrangetto5@gmail.com>
2026-02-18 19:59:09 -0300

    feat: new icon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,26 @@ import { Link } from "react-router";
 export default function Home() {
   return (
     <>
-      <h1>Welcome to Neomall</h1>
-      <Link to='/login'>Login </Link>
-      <Link to='/register'>Register</Link>
+      <main className='home-main'>
+        <title>NeoMall</title>
+        <section className='home-section'>
+          <div className='home-presentation'>
+            <div className='home-icon-title'>
+              <img src='/icon.svg' alt='app icon' className='home-icon' />
+              <h3>NeoMall</h3>
+            </div>
+            <div className='home-name-presentation'>
+              <h2 className='home-title'>
+                Your shopping <br></br> experience, redefined.
+              </h2>
+              <p>The future of online shopping, simplified for you</p>
+            </div>
+          </div>
+          <Link to='/products' className='btn'>
+            Explore products
+          </Link>
+        </section>
+      </main>
     </>
   );
 }
