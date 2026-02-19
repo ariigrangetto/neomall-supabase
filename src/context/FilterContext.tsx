4c00d9edea89f698a8a51c9554bb2c788216bd64
Arianna <ariigrangetto5@gmail.com>
2026-02-19 11:37:26 -0300

    feat: updating filters context

--- src/context/FilterContext.tsx
+++ src/context/FilterContext.tsx
@@ -1,9 +1,12 @@
 import React, { createContext, useState } from "react";
 import type { FiltersType } from "../types.d";
+import { useSearchParams, type SetURLSearchParams } from "react-router";
 
 interface FilterContextType {
   filters: FiltersType;
   setFilters: React.Dispatch<React.SetStateAction<FiltersType>>;
+  searchParams: URLSearchParams;
+  setSearchParams: SetURLSearchParams;
 }
 export const FilterContext = createContext<FilterContextType | undefined>(
   undefined,
@@ -14,13 +17,16 @@ interface FilterProviderProp {
 }
 
 export default function FilterProvider({ children }: FilterProviderProp) {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [filters, setFilters] = useState<FiltersType>({
-    text: "",
-    category: "",
+    text: searchParams.get("title") || "",
+    category: searchParams.get("category") || "",
   });
 
   return (
-    <FilterContext.Provider value={{ filters, setFilters }}>
+    <FilterContext.Provider
+      value={{ filters, setFilters, searchParams, setSearchParams }}
+    >
       {children}
     </FilterContext.Provider>
   );
