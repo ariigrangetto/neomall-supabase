18d8f942cb17d1195a949fcf3234403221a31ac3
Arianna <ariigrangetto5@gmail.com>
2026-03-09 12:05:13 -0300

    feat: new not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router";
+import Footer from "../components/Footer";
 
 export default function NotFound() {
   return (
     <>
       <header>
         <h1>404</h1>
-        <h2>Not found</h2>
-        <p>It looks like theres been an error</p>
-        <Link to='/'>Return to NeoMall</Link>
+        <h2>Page not found</h2>
+        <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+        <Link to='/'>Back to home</Link>
       </header>
+      <Footer />
     </>
   );
 }
