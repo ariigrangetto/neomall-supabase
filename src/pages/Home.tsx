0e7663358695188555c6ecd4b16dae42177377e3
Arianna <ariigrangetto5@gmail.com>
2026-03-10 11:08:00 -0300

    feat: new icon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,20 +9,20 @@ export default function Home() {
           <title>NeoMall</title>
           <div className='home-icon-title flex items-center mb-8 justify-center  mt-60 '>
             <img
-              src='/icon.svg'
+              src='/iconNeomallN.jpeg'
               alt='app icon'
               className='home-icon justify-center flex m-auto w-36  '
             />
-            <h3 className='text-black items-center mt-2 font-bold text-4xl'>
+            <h3 className='text-white items-center mt-2 font-bold text-4xl'>
               NeoMall
             </h3>
           </div>
           <section className='home-section flex flex-col items-center text-center '>
             <div className='home-name-presentation relative'>
-              <h2 className='home-title text-3xl font-bold mb-4 text-black '>
+              <h2 className='home-title text-3xl font-bold mb-4 text-white '>
                 Your shopping <br></br> experience, redefined.
               </h2>
-              <p className='text-black'>
+              <p className='text-white'>
                 The future of online shopping, simplified for you.
               </p>
             </div>
@@ -31,6 +31,12 @@ export default function Home() {
               className='btn mt-6 px-6 py-2 bg-blue-500 rounded text-white border-0 pointer  hover:bg-blue-600 hover:text-white hover:border-0 hover:duration-500'
             >
               Explore products
+            </Link>
+            <Link
+              to='/login'
+              className='btn mt-6 px-6 py-2 bg-blue-500 rounded text-white border-0 pointer  hover:bg-blue-600 hover:text-white hover:border-0 hover:duration-500'
+            >
+              Login
             </Link>
           </section>
         </main>
