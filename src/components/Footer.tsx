ed30ec9c21c0022cc769a0bfc6515ccbc3f8b0df
Arianna <ariigrangetto5@gmail.com>
2026-03-10 11:08:44 -0300

    feat: change text color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className='w-full py-4 flex items-center justify-center'>
-      <small className='text-black text-sm'>
+      <small className='text-white text-sm'>
         &copy; {year} NeoMall. All rights reserved
       </small>
     </footer>
