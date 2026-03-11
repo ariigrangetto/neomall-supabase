482a4ce6ceb536ea5cc6d540d0866d88bd04f0e2
Arianna <ariigrangetto5@gmail.com>
2026-03-11 12:16:20 -0300

    feat: adding register data

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,19 +1,21 @@
 import { useRef, useState } from "react";
 import { Link, useNavigate } from "react-router";
 import supabase from "../supabase/client.js";
-import useLoading from "../hooks/useLoadingAndError.js";
 
 interface State {
   email: string;
   password: string;
+  name: string;
+  lastname: string;
 }
 
 export default function Register() {
-  const [state, setState] = useState<State>({ email: "", password: "" });
+  const [state, setState] = useState<State>({ email: "", password: "", name: "", lastname: "" });
   const [errorMessage, setErrorMessage] = useState<string>("");
   let timeoutId = useRef<number | null>(null);
   const navigate = useNavigate();
-  const { loading, setLoading } = useLoading();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [authenticateMessage, setAuthenticateMessage] = useState<string>("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     setLoading(true);
@@ -21,10 +23,14 @@ export default function Register() {
     const { error } = await supabase.auth.signUp({
       email: state.email,
       password: state.password,
+      options: {
+        data: {
+          fist_name: state.name,
+          last_name: state.lastname
+        }
+      }
     });
 
-    console.log(error);
-    setLoading(false);
     if (error) {
       setErrorMessage(
         error.message.includes("rate limit")
@@ -37,10 +43,16 @@ export default function Register() {
       timeoutId.current = setTimeout(() => {
         setErrorMessage("");
       }, 3000);
+      setLoading(false);
       return;
     }
+    setAuthenticateMessage("Please, check your email and validate your account.");
     setLoading(false);
-    navigate("/products");
+    await supabase.auth.onAuthStateChange((event, session) => {
+      if (session) {
+        navigate("/products");
+      }
+    });
   };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -56,10 +68,28 @@ export default function Register() {
   return (
     <>
       <div>
-        <h1>Registrarse</h1>
+        <h1>Register</h1>
       </div>
       <div>
         <form onSubmit={handleSubmit}>
+          <label htmlFor='name'>Name</label>
+          <input
+            id='name'
+            type='text'
+            name='name'
+            required
+            placeholder='your name'
+            onChange={onChange}
+          />
+          <label htmlFor='lastname'>Lastname</label>
+          <input
+            id='lastname'
+            type='text'
+            name='lastname'
+            required
+            placeholder='your lastname'
+            onChange={onChange}
+          />
           <label htmlFor='email'>Email</label>
           <input
             id='email'
@@ -78,15 +108,17 @@ export default function Register() {
             required
             onChange={onChange}
           />
-          <button type='submit'>
+          <button type='submit' className="text-white">
             {loading ? "Registrando..." : "Registrarse"}
           </button>
         </form>
         {errorMessage && <p>{errorMessage}</p>}
+        {authenticateMessage && <p>{authenticateMessage}</p>}
+
       </div>
       <div>
-        <span>¿Ya tienes una cuenta? </span>
-        <Link to='login'>Iniciar sesión</Link>
+        <span>Already have an account? </span>
+        <Link to='login'>Login</Link>
       </div>
     </>
   );
