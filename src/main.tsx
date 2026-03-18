74dcff3a2fc39148c7d5dde2ecf3966f151141ea
Arianna <ariigrangetto5@gmail.com>
2026-03-18 10:40:06 -0300

    feat: delete unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
-import FilterProvider from "./context/FilterContext.tsx";
 import { router } from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />,
+  <RouterProvider router={router} />
 );
