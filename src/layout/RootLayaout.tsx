f480fbd7682bab1d265ce88aba82a4be1684388f
Arianna <ariigrangetto5@gmail.com>
2026-03-20 10:46:58 -0300

    feat: add userProvider

--- src/layout/RootLayaout.tsx
+++ src/layout/RootLayaout.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { Outlet } from "react-router";
 import FilterProvider from "../context/FilterContext.tsx";
 import LoadingProvider from "../context/LoadingErrorContext.tsx";
 import CartProvider from "../context/CartContext.tsx";
+import UserProvider from "../context/UserActions.tsx";
 
 export default function RootLayout() {
   return (
     <FilterProvider>
       <LoadingProvider>
         <CartProvider>
-          <Outlet />
+          <UserProvider>
+            <Outlet />
+          </UserProvider>
         </CartProvider>
       </LoadingProvider>
     </FilterProvider>
