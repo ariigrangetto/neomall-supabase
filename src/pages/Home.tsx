3da341e4943d0df7b2b7eebb80ef7bd922f0d0ad
Arianna Grangetto <ariigrangetto5@gmail.com>
2026-03-23 11:30:03 -0300

    feat: new styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,22 +18,24 @@ export default function Home() {
 
   return (
     <>
+      <title>NeoMall</title>
       <div className='min-h-screen flex flex-col'>
-        <main className='home-main flex flex-col flex-1  items-center overscroll-y-none mb-0'>
-          <title>NeoMall</title>
-          <div className='home-icon-title flex items-center mb-8 justify-center  mt-60 '>
-            <img
-              src='/iconN.png'
-              alt='app icon'
-              className='home-icon justify-center flex m-auto w-36  '
-            />
-            <h3 className='text-white items-center mt-2 font-bold text-4xl'>
+        <main className='flex-grow flex flex-col items-center justify-center p-4'>
+          <div className='flex items-center mb-8 justify-center gap-2'>
+            <button onClick={() => navigate("/")} className='outline-none border-none cursor-pointer'>
+              <img
+                src='/iconN.png'
+                alt='app icon'
+                className='w-36'
+              />
+            </button>
+            <h3 className='text-white font-bold text-4xl'>
               NeoMall
             </h3>
           </div>
-          <section className='home-section flex flex-col items-center text-center '>
-            <div className='home-name-presentation relative'>
-              <h2 className='home-title text-3xl font-bold mb-4 text-white '>
+          <section className='flex flex-col items-center text-center'>
+            <div className='relative'>
+              <h2 className='text-3xl font-bold mb-4 text-white '>
                 Your shopping <br></br> experience, redefined.
               </h2>
               <p className='text-white'>
@@ -41,20 +43,17 @@ export default function Home() {
               </p>
             </div>
           </section>
-          <section>
-
-            <div className="mt-10 justify-center flex m-auto">
-              <button
-                onClick={handleClickProducts}
-                className='btn h-8 border border-[rgba(7,75,248,1)] w-60  rounded-full text-white  pointer  hover:bg-[rgba(7,75,248,0.8)] hover:text-white hover:border-0 hover:duration-500'
-              >
-                Explore products
-              </button>
-            </div>
+          <section className='flex flex-col items-center mt-10'>
+            <button
+              onClick={() => handleClickProducts()}
+              className='h-10 border border-[rgba(7,75,248,1)] w-60 rounded-full text-white cursor-pointer hover:bg-[rgba(7,75,248,0.8)] hover:text-white hover:border-0 hover:duration-500 transition-all font-semibold'
+            >
+              Explore products
+            </button>
 
             <div className="flex gap-3 mt-5 justify-center">
               <p>Already have an account?</p>
-              <p className="text-blue-500 cursor-pointer"><Link to="/login">Login</Link></p>
+              <p className="text-blue-500 cursor-pointer font-semibold"><Link to="/login">Login</Link></p>
             </div>
           </section>
         </main>
