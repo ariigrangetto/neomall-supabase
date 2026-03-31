1322d9618f8816667693f563a607b7382b01cd9c
Arianna <ariigrangetto5@gmail.com>
2026-03-31 11:54:07 -0300

    fix: props and classname

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,28 +1,32 @@
 /* eslint-disable react/display-name */
-import type { CartItem, Products } from "../types.d.ts";
+import type { Products } from "../types.d.ts";
 import React from "react";
 import { Heart, HeartCrack, MousePointerClick, ShoppingCart } from "lucide-react";
 import { Link, useNavigate } from "react-router";
-import useCartActions from "../hooks/useCartActions.tsx";
-
 interface ProductCartProps {
     product: Products;
-    inCartInfo: { className: string, inCart: boolean | CartItem };
+    inCart: boolean;
+    isFav: boolean;
     isLoading: boolean;
     isAuthenticated: boolean;
+    addProductToCart: (id: string | number) => void;
+    addToFavorites: (id: string | number) => void;
+    deleteFromFavorites: (id: string | number) => void;
+    deleteProductFromCart: (id: string | number) => void;
 }
 
 export const ProductCard = React.memo(({
-    product, inCartInfo, isLoading, isAuthenticated
+    product, inCart, isFav, isLoading, isAuthenticated,
+    addProductToCart, addToFavorites, deleteFromFavorites, deleteProductFromCart
 }: ProductCartProps) => {
-    const { wishListMap, addProductToCart, addToFavorites, deleteFromFavorites, deleteProductFromCart } = useCartActions();
     const navigate = useNavigate();
 
     const handleClickLogin = () => {
         navigate("/login");
     }
 
-    const isFav = wishListMap?.has(product.id) ?? false;
+    const inCartClassName = inCart ? "flex items-center w-full justify-center text-center gap-2 text-white bg-[rgba(0,150,32,1)] font-semibold cursor-pointer border-0 outline-0 hover:text-gray-200 hover:bg-[#007a1a] h-10 px-10 rounded transition-colors duration-300"
+        : "flex items-center w-full justify-center text-center gap-2 text-white bg-[rgba(7,75,248,1)] font-semibold cursor-pointer border-0 outline-0 hover:text-gray-200 hover:bg-[#0335b4] h-10 px-10 rounded transition-colors duration-300";
 
     return (
         <li className="flex bg-[rgba(23,23,23,1)] rounded text-white " key={product.id}>
@@ -61,17 +65,17 @@ export const ProductCard = React.memo(({
                 <div className="flex justify-center mt-auto py-6">
                     {isAuthenticated ? (
                         <div className="flex justify-center flex-col gap-5 w-[300px]">
-                            {!inCartInfo.inCart ? (
+                            {inCart ? (
                                 <div className="flex items-center justify-center">
-                                    <button onClick={() => addProductToCart(product.id)} className={inCartInfo.className}>
-                                        {isLoading ? "Adding to cart..." : "Add to cart"}
+                                    <button onClick={() => deleteProductFromCart(product.id)} className="flex items-center w-full justify-center text-center gap-2 text-white bg-[rgba(204,0,0,0.78)] font-semibold cursor-pointer border-0 outline-0 hover:text-gray-200 hover:bg-[rgba(119,0,0,1)] h-10 px-10 rounded transition-colors duration-300">
+                                        Remove from cart
                                         <ShoppingCart size={20} />
                                     </button>
                                 </div>
                             ) : (
                                 <div className="flex items-center justify-center">
-                                    <button onClick={() => deleteProductFromCart(product.id)} className="flex items-center w-full justify-center text-center gap-2 text-white bg-[rgba(204,0,0,0.78)] font-semibold cursor-pointer border-0 outline-0 hover:text-gray-200 hover:bg-[rgba(119,0,0,1)] h-10 px-10 rounded transition-colors duration-300">
-                                        {isLoading ? "Removing from cart..." : "Remove from cart"}
+                                    <button onClick={() => addProductToCart(product.id)} className={inCartClassName}>
+                                        {isLoading ? "Adding to cart..." : "Add to cart"}
                                         <ShoppingCart size={20} />
                                     </button>
                                 </div>
@@ -94,4 +98,10 @@ export const ProductCard = React.memo(({
             </div>
         </li >
     )
-})
+}, (prevProps, nextProps) => {
+    return prevProps.product.id === nextProps.product.id &&
+        prevProps.inCart === nextProps.inCart &&
+        prevProps.isFav === nextProps.isFav &&
+        prevProps.isLoading === nextProps.isLoading &&
+        prevProps.isAuthenticated === nextProps.isAuthenticated;
+});
