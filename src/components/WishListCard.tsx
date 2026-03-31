a9cc23564c44368939cc6b2be16870082431c8d3
Arianna <ariigrangetto5@gmail.com>
2026-03-31 11:54:22 -0300

    feat: add wishList section

---
+++ src/components/WishListCard.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { HeartCrack } from "lucide-react";
+import type { Products } from "../types";
+
+interface WishListCardProps {
+    product: Products;
+    key: string | number;
+    deleteFromFavorites: (id: string | number) => void;
+}
+
+export default function WishListCard({ product, key, deleteFromFavorites }: WishListCardProps) {
+    return (
+        <li className="flex bg-[rgba(23,23,23,1)] rounded text-white" key={key}>
+            <div className="w-100 rounded shadow-md overflow-hidden flex flex-col">
+                <div className="relative flex justify-center p-4 h-64 w-full">
+                    <img src={product.image} alt={product.title} />
+                </div>
+                <div className='description flex flex-col items-center justify-center p-4 shrink-0'>
+                    <h2 className="text-center font-bold text-[21px] leading-tight mb-1">{product.title}</h2>
+                    <h3 className="text-center text-1xl text-gray-300 capitalize mb-2">{product.category}</h3>
+                    <strong className="text-center text-xl">${product.price}</strong>
+                </div>
+                <div className="p-2 px-6 grow flex items-center justify-center text-center">
+                    <p className="text-[16px] text-justify line-clamp-4">{product.description}</p>
+                </div>
+                <div className="flex justify-center mt-auto py-6">
+                    <button onClick={() => deleteFromFavorites(product.id)} className="flex items-center w-full justify-center text-center gap-2 text-white bg-[rgba(204,0,0,0.78)] font-semibold cursor-pointer border-0 outline-0 hover:text-gray-200 hover:bg-[rgba(119,0,0,1)] h-10 px-10 rounded transition-colors duration-300">
+                        Remove from wishlist
+                        <HeartCrack size={20} />
+                    </button>
+                </div>
+            </div>
+        </li >
+    )
+}
---
+++ src/pages/WishList.tsx
@@ -0,0 +1,68 @@
+import { ArrowLeft, Frown } from "lucide-react";
+import { Link, useNavigate } from "react-router";
+import useCartActions from "../hooks/useCartActions";
+import WishListCard from "../components/WishListCard";
+
+/* eslint-disable react/react-in-jsx-scope */
+export default function WishList() {
+    const navigate = useNavigate();
+    const { wishUserList, deleteFromFavorites } = useCartActions();
+    console.log(wishUserList);
+
+    const handleRedirectProducts = () => {
+        navigate("/products");
+    }
+
+    return (
+        <main className="min-h-screen">
+            <title>WishList</title>
+            <div className="flex flex-wrap items-center justify-between w-full gap-3 p-4 sm:p-6">
+                <div className="flex items-center shrink-0">
+                    <nav className="flex items-center text-center gap-2">
+                        <button className="cursor-pointer" onClick={() => handleRedirectProducts()}><ArrowLeft /></button>
+                        <button className="cursor-pointer" onClick={() => navigate("/")}>
+                            <img src="/iconN.png" alt="ICON IMAGE" className="h-8" />
+                        </button>
+                        <h1 className="font-bold text-xl">Neomall</h1>
+                    </nav>
+                </div>
+                <div className="flex items-center shrink-0 gap-3">
+                    <nav className="w-10 text-center">
+                        <Link to="/profile">
+                            <img src="/profilePic.png" className="h-9 rounded-full" alt="USER PROFILE PIC" />
+                        </Link>
+                    </nav>
+                </div>
+            </div>
+            {
+                wishUserList === undefined || wishUserList.length === 0 ? (
+                    <>
+                        <div className="flex flex-col items-center justify-center min-h-[60vh] gap-4 p-6 sm:p-8 text-center">
+                            <div className="p-4 bg-blue-500/10 rounded-full">
+                                <Frown className="w-12 h-12 text-blue-500" />
+                            </div>
+                            <h1 className="text-2xl sm:text-[30px] font-bold text-white">Oops! Looks like your wishlist is empty.</h1>
+                            <p className="text-gray-400 text-base sm:text-lg max-w-md">Start adding products to your wishlist to fill it up!</p>
+                            <button
+                                onClick={() => handleRedirectProducts()}
+                                className="mt-4 px-6 py-2 bg-[rgba(7,75,248,1)] text-white font-semibold rounded-full hover:bg-[#0335b4] transition-colors cursor-pointer"
+                            >
+                                Start Shopping
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 sm:gap-8 p-4 sm:p-8 md:p-12 justify-items-center sm:justify-items-stretch">
+                        {wishUserList.map((item) => (
+                            <WishListCard
+                                key={item.id}
+                                product={item.Products}
+                                deleteFromFavorites={deleteFromFavorites}
+                            />
+                        ))}
+                    </ul>
+                )
+            }
+        </main>
+    )
+}
