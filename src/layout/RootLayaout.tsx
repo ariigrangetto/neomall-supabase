67b9b85b974df4f670c9df6f390e15538f9ba5d4
Arianna <ariigrangetto5@gmail.com>
2026-03-31 11:54:54 -0300

    feat: add suspense

--- src/layout/RootLayaout.tsx
+++ src/layout/RootLayaout.tsx
@@ -4,6 +4,8 @@ import FilterProvider from "../context/FilterContext.tsx";
 import LoadingProvider from "../context/LoadingErrorContext.tsx";
 import CartProvider from "../context/CartContext.tsx";
 import UserProvider from "../context/UserActions.tsx";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
 
 export default function RootLayout() {
   return (
@@ -11,7 +13,14 @@ export default function RootLayout() {
       <LoadingProvider>
         <CartProvider>
           <UserProvider>
-            <Outlet />
+            <Suspense fallback={
+              <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+                <Loader2 className="w-12 h-12 text-[rgba(7,75,248,1)] animate-spin" />
+                <h1 className="mt-4 text-xl font-medium text-gray-300">Loading...</h1>
+              </div>
+            }>
+              <Outlet />
+            </Suspense>
           </UserProvider>
         </CartProvider>
       </LoadingProvider>
